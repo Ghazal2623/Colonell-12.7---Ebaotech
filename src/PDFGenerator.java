6d19b554c387e1020c6b31aa90b581e7870425dc
Ghazal2623 <ghazal.ashraf24@gmail.com>
2025-08-12 00:06:01 +0400

    Update PDFGenerator.java

--- src/PDFGenerator.java
+++ src/PDFGenerator.java
@@ -1,1 +1,9 @@
+public class PDFGenerator {
+    public void generatePDF(String content, String filename) {
+        // Simulate PDF generation
+        System.out.println("Generating PDF: " + filename);
+        System.out.println("Content: " + content);
+        System.out.println("PDF generated successfully!");
+    }
+}
 
