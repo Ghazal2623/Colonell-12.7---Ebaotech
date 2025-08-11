46600d1ed29e3a6749a78a36b81ee713a201ae8a
Ghazal2623 <ghazal.ashraf24@gmail.com>
2025-08-12 00:06:17 +0400

    Update SecureDataExchange.java

--- src/SecureDataExchange.java
+++ src/SecureDataExchange.java
@@ -1,1 +1,33 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class SecureDataExchange {
+
+    private SecretKey secretKey;
+
+    public SecureDataExchange() throws Exception {
+        // Generate secret key for AES encryption
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        secretKey = keyGen.generateKey();
+    }
+
+    public String encrypt(String data) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decoded = Base64.getDecoder().decode(encryptedData);
+        byte[] decrypted = cipher.doFinal(decoded);
+        return new String(decrypted);
+    }
+}
 
